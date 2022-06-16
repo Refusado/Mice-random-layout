da502ec0e717580828688a03aebefd557bb1066b
Renan Freitas <89546855+Refusado@users.noreply.github.com>
2022-06-15 23:16:21 -0300

    v6 - Sistema de grid
    Adicionado sistema de grid e sistema de sistema de proporções para que cada piso tenha seu tamanho e posicionamento lógico

--- index.php
+++ index.php
@@ -1,35 +1,101 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
+  <link rel="icon" type="image/x-icon" href="./images/settings-icon.svg">
   <title>Layout Generator - TFM maps</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <main>
     <svg viewBox="0 0 800 400">
-<?php
-session_start();
+    <?php
+function generateNewGround($id) {
+  global $grounds;
 
-if (isset($_GET['grounds-number']) && $_GET['grounds-number'] != 0) {
-  $groundsNo = $_GET['grounds-number'];
+  global $proportionMult;
+  global $positionMult;
+
+  global $groundWidth;
+  global $groundHeight;
+  
+  global $groundOriginX;
+  global $groundOriginY;
+
+  global $groundEndX;
+  global $groundEndY;
+
+  global $maxGroundWidth;
+  global $maxGroundHeight;
+
+  $groundWidth = $grounds[$id]['width'] =  gMult(10, $maxGroundWidth, $proportionMult);
+  $groundHeight = $grounds[$id]['height'] = gMult(10, $maxGroundHeight, $proportionMult);
+  
+  $groundOriginX = $grounds[$id]['originX'] = gMult(0, (800 - $grounds[$id]['width']), $positionMult);
+  $groundOriginY = $grounds[$id]['originY'] = gMult(0, (400 - $grounds[$id]['height']), $positionMult);
+  
+  $groundEndX = $grounds[$id]['endX'] = $groundOriginX + $groundWidth;
+  $groundEndY = $grounds[$id]['endY'] = $groundOriginY + $groundHeight;
 }
-$_SESSION['lastGroundsNo'] = $groundsNo = $_GET['grounds-number'] ?? $_SESSION['lastGroundsNo'] ?? 5;
 
-if (@$_GET['edited']) {
+function verifyException($var){
+  global $typeExceptions;
+  if (!is_null($typeExceptions)){
+    foreach ($typeExceptions as $execption) {
+      if ($var == $execption){
+        return true;
+      }
+    }
+  }
+}
+function verifyDisable($var){
+  global $typeDisable;
+  if (!is_null($typeDisable)){
+    foreach ($typeDisable as $disable) {
+      if ($var == $disable){
+        return true;
+      }
+    }
+  }
+}
+function gMult($min, $max, $m) {
+  $n = random_int($min, $max);
+  while (($n % $m) != 0) {
+      $n = random_int($min, $max);
+  }
+  return $n;
+}
+
+$typeDisable = array(13, 14);
+
+$typeColors = array(
+  "#324650","#89A7F5","#6D4E94","#D84801","#2E190C","#324650","#324650","#324650","#f3faf86b","#6d9fb85d","#324650","#E7F0F2","#324650","#324650","#00000000","#f3faf86b","#324650","#324650","#324650","#51A317"
+);
+
+$proportionMult = $positionMult = 80;
+// Múltiplos de 400 > 200, 100, 80, 50, 40, 25, 20, 16, 10, 8, 5, 4, 2, 1
+
+$maxGroundWidth = 400;
+$maxGroundHeight = 200;
+
+if (isset($_POST['grounds-number']) && $_POST['grounds-number'] != 0) {
+  $groundsNo = $_POST['grounds-number'];
+}
+$_SESSION['lastGroundsNo'] = $groundsNo = $_POST['grounds-number'] ?? $_SESSION['lastGroundsNo'] ?? 4;
+
+if (@$_POST['edited']) {
   $typeToShow = array();
   $typeExceptions = array();
 
   for ($i = 0; $i < 20; $i++) {
     array_push($typeToShow, $i);
-    if (isset($_GET['e-' . $i])) {
+    if (isset($_POST['e-' . $i])) {
       array_push($typeExceptions, $i);
     }
   }
 
   $_SESSION['lastExceptions'] = $typeExceptions;
-  header('location: ./');
-
 } else {
   $typeExceptions = $_SESSION['lastExceptions'] ??
   array(
@@ -37,19 +103,8 @@ if (@$_GET['edited']) {
   );
 }
 
-$typeColors = array(
-  "#324650","#89A7F5","#6D4E94","#D84801","#2E190C","#324650","#324650","#324650","#f3faf86b","#6d9fb85d","#324650","#E7F0F2","#324650","#324650","#00000000","#f3faf86b","#324650","#324650","#324650","#51A317"
-);
-
 for ($i = 0; $i < $groundsNo; $i++) { 
-  $groundWidth = $grounds[$i]['width'] = random_int(20, 400);
-  $groundHeight = $grounds[$i]['height'] = random_int(20, 200);
-
-  $groundOriginX = $grounds[$i]['originX'] = random_int(0, (800 - $grounds[$i]['width']));
-  $groundEndX = $grounds[$i]['endX'] = $groundOriginX + $groundWidth;
-  
-  $groundOriginY = $grounds[$i]['originY'] = random_int(0, (400 - $grounds[$i]['height']));
-  $groundEndY = $grounds[$i]['endY'] = $groundOriginY + $groundHeight;
+  generateNewGround($i);
   
   if ($i > 0) {
     for ($ii = 0; $ii < $i; $ii++) {
@@ -65,15 +120,7 @@ for ($i = 0; $i < $groundsNo; $i++) {
           $grounds[$i]['endY'] > $grounds[$ii]['originY'] &&
           $grounds[$i]['originY'] < $grounds[$ii]['endY']
           ) {
-            $groundWidth = $grounds[$i]['width'] = random_int(20, 400);
-            $groundHeight = $grounds[$i]['height'] = random_int(20, 200);
-            
-            $groundOriginX = $grounds[$i]['originX'] = random_int(0, (800 - $grounds[$i]['width']));
-            $groundEndX = $grounds[$i]['endX'] = $groundOriginX + $groundWidth;
-            
-            $groundOriginY = $grounds[$i]['originY'] = random_int(0, (400 - $grounds[$i]['height']));
-            $groundEndY = $grounds[$i]['endY'] = $groundOriginY + $groundHeight;
-            
+            generateNewGround($i);
             $ii = -1;
         }
       }
@@ -81,105 +128,95 @@ for ($i = 0; $i < $groundsNo; $i++) {
   }
 
   $grounds[$i]['type'] = random_int(0, count($typeColors) - 1);
-  if ($typeExceptions < $typeColors) {
+  if (count($typeExceptions) < count($typeColors)) {
     for ($ii = 0; $ii < count($typeExceptions); $ii++) { 
-      if($grounds[$i]['type'] == $typeExceptions[$ii]) {
+      if ($grounds[$i]['type'] == $typeExceptions[$ii]) {
         $grounds[$i]['type'] = random_int(0, count($typeColors) - 1);
         $ii = -1;
+      }
+
+      for ($iii = 0; $iii < count($typeDisable); $iii++) { 
+        if ($grounds[$i]['type'] == $typeDisable[$iii]) {
+          $grounds[$i]['type'] = random_int(0, count($typeColors) - 1);
+          $ii = -1;
+        }
       }
     };
     $groundColor = $typeColors[$grounds[$i]['type']];
 
-    echo "
-    <rect width='$groundWidth' height='$groundHeight' x='$groundOriginX' y='$groundOriginY' fill='$groundColor'/>
+        echo "  <rect width='$groundWidth' height='$groundHeight' x='$groundOriginX' y='$groundOriginY' fill='$groundColor'/>
     ";
   } else {
     break;
   }
 };
 ?>
-    <rect x="0" y="0" width="100%" height="22px" fill="#00000015"/>
-  </svg>
+</svg>
   <div class="s">
-    <div class="btns-container">
-      <a class="btn" id="generate" href="./">
-        <img src="./images/refresh-icon.svg" alt="Generate new layout">
-      </a>
-      <label class="btn" id="show-settings-btn" for="show-settings">
-        <img src="./images/settings-icon.svg" alt="Settings">
-      </label>
-    </div>
-    
-    <input type="checkbox" name="show-settings" id="show-settings">
-    <form id="map-settings" action="" method="get">
-      <input type="hidden" name="edited" value="true">
-      <div class='types-container'>
-
-      <?php
-      $groundTypes = array(
-        "Madeira","Gelo","Trampolim","Lava","Chocolate","Terra","Grama","Areia","Nuvem","Água","Pedra","Neve","Retângulo","Circulo","Invisível","Teia","Madeira II","Grama II","Grama III","Ácido"
-      );
-
-      function verifyException($var){
-        global $typeExceptions;
-        if (!is_null($typeExceptions)){
-          foreach ($typeExceptions as $execption) {
-            if ($var == $execption){
-              return true;
-            }
-          }
-        }
-      }
-      for ($i = 0; $i < count($groundTypes); $i++) {
-        if (verifyException($i)) {
-          echo "
-          <input type='checkbox' name='e-$i' id='e-$i' checked>
-          <label class='ground-btn' for='e-$i'>{$groundTypes[$i]}</label>
-          ";
-        } else {
-          echo "
-          <input type='checkbox' name='e-$i' id='e-$i'>
-          <label class='ground-btn' for='e-$i'>{$groundTypes[$i]}</label>
-          ";
-        }
-      }
-      ?>
-      </div>
-
-      <div id="btns-on-settings">
-        <label class="btn" id="close-settings-btn" for="show-settings">
-          <img src="./images/back-icon.svg" alt="Close settings">
+    <form action="" method="post">
+      <div class="btns-container">
+        <input id="save-btn" type="submit" value="Salver configurações">
+        <label class="btn" id="close-settings-btn" for="save-btn">
+          <img src="./images/refresh-icon.svg" alt="Generate new layout">
         </label>
+        <label class="btn" id="show-settings-btn" for="show-settings">
+          <img src="./images/settings-icon.svg" alt="Settings">
+        </label>
+      </div>
+      <input type="checkbox" name="show-settings" id="show-settings">
+      <div id="map-settings">
+        <input type="hidden" name="edited" value="true">
+        <div class='types-container'>
+          <?php
+          $groundTypes = array(
+            "Madeira","Gelo","Trampolim","Lava","Chocolate","Terra","Grama","Areia","Nuvem","Água","Pedra","Neve","Retângulo","Circulo","Invisível","Teia","Madeira II","Grama II","Grama III","Ácido"
+          );
 
-        <div class='grounds-no-container'>
-          <p style="text-align: center;">Pisos</p>
-
-          <?php echo "
-          <input type='range' name='grounds-number' id='grounds-number' step='1' value='$groundsNo' min='1' max='10'>
-          
-          <div class='steps-line'>";
-          for ($i = 1; $i <= 10; $i++) { 
-            echo "
-            <span>|</span>
-            ";
-            if ($i == 10){
-              echo "</div>
-              <div class='steps-number'>";
-              for ($ii = 1; $ii <= 10; $ii++) { 
-                echo "<span>$ii</span>";
-                if ($ii == 10){
-                  echo "</div>";
-                }
-              } 
+          for ($i = 0; $i < count($groundTypes); $i++) {
+            if (verifyException($i) && verifyException($i) != verifyDisable($i)) {
+              echo "<input type='checkbox' name='e-$i' id='e-$i' checked>
+              <label class='ground-btn' for='e-$i'>{$groundTypes[$i]}</label>
+              ";
+            } else if (verifyDisable($i)) {
+              echo "<input type='checkbox' name='e-$i' id='e-$i'>
+              <label class='ground-btn disable' for='e-$i'>{$groundTypes[$i]}</label>
+              ";
+            } else {
+              echo "<input type='checkbox' name='e-$i' id='e-$i'>
+              <label class='ground-btn' for='e-$i'>{$groundTypes[$i]}</label>
+              ";
             }
           }
           ?>
-
         </div>
-        <input id="save-btn" type="submit" value="Salver configurações">
-        <label class="btn" id="close-settings-btn" for="save-btn">
-          <img src="./images/save-icon.svg" alt="Save settings">
-        </label>
+
+        <div id="btns-on-settings">
+          <div class='grounds-no-container'>
+            <p style="text-align: center;">Pisos</p>
+            <?php
+            echo "
+            <input type='range' name='grounds-number' id='grounds-number' step='1' value='$groundsNo' min='1' max='10'>
+            <div class='steps-line'>";
+            for ($i = 1; $i <= 10; $i++) { 
+              echo "<span>|</span>
+              ";
+              if ($i == 10){
+                echo "</div>
+                <div class='steps-number'>
+                ";
+                for ($ii = 1; $ii <= 10; $ii++) { 
+                  echo "<span>$ii</span>
+                  ";
+                  if ($ii == 10){
+                    echo "</div>";
+                  }
+                } 
+              }
+            }
+            ?>
+
+          </div>
+        </div>
       </div>
     </form>
   </div>
